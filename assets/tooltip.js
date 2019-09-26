f94870f2a1072a5273fb6ae0c943922d8a137c22
harmp988 <harmp98@gmail.com>
2019-09-26 22:50:59 +0200

    Updated tooltip.html style

--- assets/tooltip.js
+++ assets/tooltip.js
@@ -3,16 +3,19 @@ window.onload = function() {
 	var tooltips = document.querySelectorAll(".tooltip");
 	// For each tooltip
 	tooltips.forEach(function(tooltip, index){
+		console.log(tooltip);
 		// Set event handlers
-		tooltip.addEventListener("mouseover", onMouseOver);
-		tooltip.addEventListener("mouseout", onMouseOut);
+		tooltip.childNodes[1].addEventListener("mouseover", onMouseOver);
+		tooltip.childNodes[1].addEventListener("mouseout", onMouseOut);
 		// Default tooltip text to the left
 		// Default css is in the middle in case no javascript is loaded
-		setDefaultTooltipPosition(tooltip);
+		setDefaultTooltipPosition(tooltip.childNodes[1]);
 	})
 }
 
 function onMouseOver(){
+	console.log(this);
+	console.log(this.style);
 	// Keep mousover position
 	this.mouseIsOver = true;
 	
@@ -22,8 +25,8 @@ function onMouseOver(){
 // Prevent tooltip from going off page
 function setCorrectTooltipPosition(current){
 	// Get positions of tooltip image and text
-	var tooltip = current.childNodes[0].getBoundingClientRect();
-	var tooltip_text = current.childNodes[1].getBoundingClientRect();
+	var tooltip = current.getBoundingClientRect();
+	var tooltip_text = current.parentNode.childNodes[3].getBoundingClientRect();
 	
 	var window_width = getWindowWidth();
 	var image_width = tooltip.width;
@@ -34,7 +37,7 @@ function setCorrectTooltipPosition(current){
 	// Example: if tooltip is to the left, make it stick out to the right to prevent it going off page
 	new_tooltip_pos = parseInt((image_position/(window_width-image_width))*(image_width-tooltip_width));
 	// Set new position
-	current.childNodes[1].style.left = parseInt(new_tooltip_pos) + "px";
+	current.parentNode.childNodes[3].style.left = parseInt(new_tooltip_pos) + "px";
 }
 
 function onMouseOut(){
@@ -49,20 +52,20 @@ function onMouseOut(){
 		if (!current.mouseIsOver){
 			setDefaultTooltipPosition(current);
 		}
-	}, 750);
+	}, 500);
 }
 
 // Defaults tooltip to the left. Prevents page from becoming wider than it has to be, which prevents bugs in tooltip position calculation
 function setDefaultTooltipPosition(current){
 	// Get positions of tooltip image and text
-	var tooltip = current.childNodes[0].getBoundingClientRect();
-	var tooltip_text = current.childNodes[1].getBoundingClientRect();
+	var tooltip = current.getBoundingClientRect();
+	var tooltip_text = current.parentNode.childNodes[3].getBoundingClientRect();
 	
 	var image_width = tooltip.width;
 	var tooltip_width = tooltip_text.width;
 	
 	// Set new position
-	current.childNodes[1].style.left = parseInt(image_width-tooltip_width) + "px";
+	current.parentNode.childNodes[3].style.left = parseInt(image_width-tooltip_width) + "px";
 }
 
 // Get current window width
